eb95353b22d3b85538585eba22ac0dacd71b403e
zhaozhiqiang <614564241@qq.com>
2019-11-28 21:28:05 +0800

    First commit

---
+++ python/python/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  python
+//
+//  Created by 赵志强 on 2019/11/28.
+//  Copyright © 2019 赵志强. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
